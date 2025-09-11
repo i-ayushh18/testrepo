fc3192e9695948a8fe34d070b931ecde38256ac3
Ayush Verma <vermaayushbly@gmail.com>
2025-09-11 22:13:02 +0530

    Apply suggested changes to path/to/file.py

--- path/to/file.py
+++ path/to/file.py
@@ -1,6 +1,2 @@
-def hello_world():
-    """Simple test function."""
-    return "Hello, World!"
-
-if __name__ == "__main__":
-    print(hello_world())
+def test_hello_world():
+    assert hello_world() == "Hello, World!"
